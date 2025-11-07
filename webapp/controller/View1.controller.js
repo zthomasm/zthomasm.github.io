b8399f94b820ce57b8cbd0e0335953272255ace6
zthomasm <thomas.maier2002@gmail.com>
2025-11-07 22:21:15 +0100

    init project

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("learninggame.controller.View1", {
+        onInit() {
+        }
+    });
+});
